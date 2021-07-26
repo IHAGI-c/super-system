e8a9ea305ad59e5d6bcc6f19bbaa6aa71d35b950
haseom <gudejr31@naver.com>
2021-07-26 16:52:42 +0900

    7/26 class_2_1

---
+++ profileapp/forms.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm
+
+from profileapp.models import profile
+
+
+class ProfileCreationForm(ModelForm):
+    class Meta:
+        model = profile
+        fields = ['image','nickname', 'message']
+        
