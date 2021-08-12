ed3c32349690aed7dac9604fe1c36e924defa9e9
haseom <gudejr31@naver.com>
2021-08-12 18:18:54 +0900

    8/12 class_2_5

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from projectapp.views import ProjectCreateView
+from projectapp.views import ProjectCreateView, ProjectDetailView
 
 app_name = 'projectapp'
 
 urlpatterns = [
     path('creat/', ProjectCreateView.as_view(), name='create'),
+    path('detail/', ProjectDetailView.as_view(), name='detail'),
+
 ]
