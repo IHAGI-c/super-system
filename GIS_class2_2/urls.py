8150c779d8e9dc73ffebb8dcd13db7a2274f3758
haseom <gudejr31@naver.com>
2021-08-19 17:47:59 +0900

    8/19 class_2_4

--- GIS_class2_2/urls.py
+++ GIS_class2_2/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('articles/', include('articleapp.urls')),
     path('commnets/', include('commentapp.urls')),
     path('projects/', include('projectapp.urls')),
+    path('subscribe/', include('subscribeapp.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
